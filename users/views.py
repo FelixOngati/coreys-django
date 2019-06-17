9c8a58380a23a54baf151dbc7184513c1f0e782a
felixongati <fegati02@gmail.com>
2019-06-17 20:26:22 +0300

    resizing profile image

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -15,10 +15,29 @@ def register(request):
             messages.success(request, f'Your Account Has Been Created! You are now able to login')
             return redirect('login')
         else:
-            form = UserRegisterForm()
+            form = UserRegisterForm(request.POST)
     return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        u_profile = ProfileUpdateForm(request.POST,
+                                      request.FILES,
+                                      instance=request.user.profile)
+        if u_form .is_valid() and u_form.is_valid():
+            u_form.save()
+            u_profile.save()
+            messages.success(request, f'Your account has been updated')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        u_profile = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'u_profile': u_profile
+    }
+
+    return render(request, 'users/profile.html', context)
